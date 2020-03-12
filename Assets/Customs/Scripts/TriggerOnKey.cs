ce17e8cba72b6e8b9e182a6b947a0af7eff48c56
Leprohon <cedric.leprohon@yahoo.fr>
2020-03-12 21:56:37 +0100

    sound effect

---
+++ Assets/Customs/Scripts/TriggerOnKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerOnKey : MonoBehaviour
+{
+
+    [SerializeField] KeyCode key;
+    [SerializeField] UnityEvent unityEvent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            unityEvent.Invoke();
+        }
+    }
+
+}
